9de962ac7f28118925f1648d1887dac0b07cf1a5
asimo10 <66620496+asimo10@users.noreply.github.com>
2020-07-20 13:13:22 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="icon" href="https://is5-ssl.mzstatic.com/image/thumb/Purple123/v4/0b/43/c6/0b43c698-fdde-0599-06c0-ac35afd3d496/AppIcon-1x_U007emarketing-0-7-0-85-220.png/246x0w.png">
+<title>IpFind</title>
+</head>
     <body>
     <style>
-    textarea { white-space: nowrap; }
+    @import url("https://fonts.googleapis.com/css?family=Inconsolata|Roboto+Mono|Ubuntu+Mono|Cutive+Mono");
+pre{
+display: inline-block;
+font-family: Ubuntu Mono, monospace !important;
+text-rendering: optimizeSpeed;
+}
+    textarea {
+  width: 1344px;
+  height: 432px;
+}
     </style>
-        <code>
-        <pre>
+<code>
 <?php
       if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
     {
@@ -19,23 +31,52 @@
     {
         $ip = $_SERVER['REMOTE_ADDR'];
     }
-        echo "Your ip is: ".$ip;    //tell your ip
+        date_default_timezone_set('Asia/Tokyo');  //set timezone to japan
+        $date = date('Y-m-d H:i:s');                 //2012-03-06 17:33:07
+        $line_break = "\r\n";    //break line value
+?>
+<pre><?php
+        echo "|  _____       ______ _           _  ";
+        echo "\r\n";
+        echo "| |_   _|     |  ____(_)         | | ";
+        echo "\r\n";
+        echo "|   | |  _ __ | |__   _ _ __   __| | ";
+        echo "\r\n";
+        echo "|   | | | '_ \|  __| | | '_ \ / _` | ";
+        echo "\r\n";
+        echo "|  _| |_| |_) | |    | | | | | (_| | ";
+        echo "\r\n";
+        echo "| |_____| .__/|_|    |_|_| |_|\__,_| ";
+        echo "\r\n";
+        echo "|       | |                          ";
+        echo "\r\n";
+        echo "|       |_|                          ";
+        echo "\r\n";
+        echo "| Your ip is: ".$ip;    //tell your ip
         echo " (ipv4)";
-        echo "\r\n";
-        echo "------------------------------------------";
-        echo "\r\n";
+        echo "| The date and time now is: ".$date;
+?>
+<div id="ipinfo"><textarea>
+<?php
         $loc = file_get_contents('https://ipapi.co/'.$ip.'/json');
         echo $loc;
-        echo "\r\n";
-        $latlong = explode(",", file_get_contents('https://ipapi.co/' . $ip . '/latlong/'));
-        $weather = file_get_contents('http://api.openweathermap.org/data/2.5/weather?lat=' . $latlong[0] . '&lon=' . $latlong[1] . '&appid=c1aa74b8bea8a4568ecac29711694f9c');
-        echo $weather;
-        echo"\r\n";
-        echo "------------------------------------------";
-        echo "\r\n";
-        echo "Made by Asimo10";
 ?>
-        </pre>
+</textarea></div></pre>
+| Click <a href="/map" target="_blank">Here</a> for Map. 
+| <a href="/legal/privacy-policy/">Privacy Policy</a>
+| <a href="/legal/terms-of-use/">Terms of Use</a>
+| <a href="/" target="_blank">Help</a>
+| <a href="/about/">About</a>
+<?php
+        echo "| Made by Asimo10";
+        $file = 'log/log.txt';  //this is the file to which the IP address will be written; name it your way.
+        $fp = fopen($file, 'a'); //open file
+        fwrite($fp, $line_break);
+        fwrite($fp, $ip . $line_break);  //write ip 
+        fwrite($fp, $date . $line_break);       //write date
+        fwrite($fp, "--------------------");   //separate       
+        fclose($fp);          //close .txt
+?>
         </code>
     </body>
 </html>
